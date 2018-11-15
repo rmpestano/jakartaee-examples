2205dcbac301c74903918a41ebbae2c02a15b802
Manfred Riem <mriem@manorrock.com>
2018-11-15 10:43:17 -0700

    Fixes issue #123 - Add a remote EJB example

--- ejb/remoteEjb/client/src/main/java/jakartaee/examples/ejb/remoteejb/client/Client.java
+++ ejb/remoteEjb/client/src/main/java/jakartaee/examples/ejb/remoteejb/client/Client.java
@@ -13,7 +13,6 @@
 package jakartaee.examples.ejb.remoteejb.client;
 
 import jakartaee.examples.ejb.remoteejb.RemoteEjb;
-import java.util.Properties;
 import javax.naming.InitialContext;
 
 /**
@@ -25,26 +24,23 @@ public class Client {
 
     /**
      * Main method.
+     * 
+     * <p>
+     *  If you are running this on Glassfish/Payara and you want to connect to
+     *  a specific host the following properties would need to be used with the
+     *  InitialContext.
+     * </p>
+     * <pre>
+     *  properties.put("java.naming.factory.initial", "com.sun.enterprise.naming.SerialInitContextFactory");
+     *  properties.put("org.omg.CORBA.ORBInitialHost", "localhost");
+     *  properties.put("org.omg.CORBA.ORBInitialPort", "3700");
+     * </pre>
      *
      * @param arguments the arguments.
      * @throws Exception when an error occurs.
      */
     public static void main(String[] arguments) throws Exception {
-        Properties properties = new Properties();
-        if (arguments.length > 0) {
-            properties.put("java.naming.factory.initial", arguments[0]);
-            properties.put("org.omg.CORBA.ORBInitialHost", arguments[1]);
-            properties.put("org.omg.CORBA.ORBInitialPort", arguments[2]);
-        } else {
-            properties.put("java.naming.factory.initial", "com.sun.enterprise.naming.SerialInitContextFactory");
-            properties.put("org.omg.CORBA.ORBInitialHost", "localhost");
-            properties.put("org.omg.CORBA.ORBInitialPort", "3700");
-        }
-        /*
-         * Everything below this comment should not have to be changed if you
-         * are using a different application-client JAR than this example does.
-         */
-        InitialContext initialContext = new InitialContext(properties);
+        InitialContext initialContext = new InitialContext();
         RemoteEjb ejb = (RemoteEjb) initialContext.lookup("java:global/remoteEjb/remoteEjbJar/remoteEjb");
         System.out.println(ejb.helloWorld());
     }
